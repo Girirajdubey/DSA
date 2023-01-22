e8a22d5564cca320d02cee5bc264b47f22710198
Dipanjon Dubey <115858630+Girirajdubey@users.noreply.github.com>
2023-01-22 18:24:23 +0530

    Update selection_sort.c

--- selection_sort.c
+++ selection_sort.c
@@ -1,62 +1,44 @@
-//Selection sort 
-/* Alogorithm 
-Step 1 − Set MIN to location 0
-Step 2 − Search the minimum element in the list
-Step 3 − Swap with value at location MIN
-Step 4 − Increment MIN to point to next element
-Step 5 − Repeat until list is sorted
-*/
-
-
-#include <stdio.h>
-void display(int arr[], int n)
-{
-    int i;
+#include<stdio.h>
+void display(int arr[] , int n ){
+    int i ; 
     printf("\n");
-    for (i = 0; i <= n - 1; i++) 
-    {
-
-        printf("%5d", *(arr + i));
+    for(i=0 ; i<=n-1 ; i++){
+        
+        printf("%5d",*(arr+i));
     }
 }
-void swap(int *a, int *b)
-{
-    int temp;
-    temp = *a;
-    *a = *b;
-    *b = temp;
-}
-void selection(int arr[], int n)
-{
-    int i, j, min;
-    for (i = 0; i < n - 1; i++)
-    {
-        printf("\n%d round", i);
-        min = i;                
-        for (j = i + 1; j < n; j++)
-        {
-            if (arr[min] > arr[j])
-            {
-                min = j;
+void Selection_sort(int arr[],int n){
+    int i , j , min ,temp ;
+    for(i=0 ; i<n ; i++){ //this loop try to findout the lowest value of respective number
+        min= arr[i];
+        printf("\n--------------");
+        for(j=i ; j<n ; j++){
+            if(min>arr[j]){
+                min=arr[j];
+                temp=arr[i];//swapping
+                arr[i]=arr[j];
+                arr[j]=temp;
             }
+            display(arr,n);
+            printf("\n+++++++++++++++++");
         }
-        swap(&arr[i], &arr[min]);
-        display(arr, n);
     }
 }
-int main()
-{
 
-    int a[100], n;
-    printf("enter the size of an array");
-    scanf("%d", &n);  
-    printf("enter the array element");
-    int i;
-    for (i = 0; i < n; i++) // taking input from user 
-    {
-        printf("\nthe index is %d : ", i);
-        scanf("%d", (a + i));
+
+int main(){
+    
+    int arr[1000];
+    int n;
+    printf("enter the number of string");
+    scanf("%d",&n);
+    for(int i = 0 ; i<n ; i++){ 
+        printf("\narray[%d]  ",i);
+        scanf("%d",&arr[i]);
     }
-    selection(a, n);
-    display(a, n);
+    
+    display(arr,n);
+    Selection_sort(arr,n);
+
+    return 0 ;
 }
